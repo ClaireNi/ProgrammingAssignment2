05905befa43aa88f683aaffeb9e5665883068e1d
ClaireNi <claireni0523@126.com>
2018-07-29 21:49:33 +0800

    Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,26 +3,16 @@
 
 ## create a matrix
 
-<<<<<<< HEAD
-x<- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2
-=======
-makeCacheMatrix <- function(x = matrix()) {
->>>>>>> parent of 32050fb... Assignment
 
-)
+x<- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
 
 ##      [,1]  [,2]
 ##[1,]  0.50 -1.00
 ##[2,] -0.25  0.75
 ## get the inverse of the matrix above
 
-<<<<<<< HEAD
 solve(x) 
-=======
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
->>>>>>> parent of 32050fb... Assignment
         ## Return a matrix that is the inverse of 'x'
 ##      [,1] [,2]
 ##[1,]    6    8
